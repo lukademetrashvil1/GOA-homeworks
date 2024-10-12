74a1f374971a40665253538b4d07893a5023eed2
lukademetrashvil1 <demetrashvililuka9@gmail.com>
2024-10-12 13:16:26 +0400

    commit

---
+++ level 43/classwork/cw.js
@@ -0,0 +1,23 @@
+const friend1 ={
+    firsname:"luka",
+    lastname:"arziani",
+    age:16,
+    height:176,
+    grade:11
+}
+const friend2 ={
+    firsname:"deme",
+    lastname:"mamukashvili",
+    age:13,
+    height:156,
+    grade:7
+}
+const friend3 ={
+    firsname:"oto",
+    lastname:"motiashvili",
+    age:14,
+    height:170,
+    grade:9
+}
+friend2.firsname="vano"
+console.log(friend1.age,friend2.firsname,friend3.height)
---
+++ level 43/classwork/cw1.js
@@ -0,0 +1,7 @@
+const p= document.getElementById('zd');
+
+
+p.textContent="zd chqim jima";
+
+p.style.color="red" 
+/*DOM არის დოკკუმენტის ობიექტის მოდელი ანუ HTML-ის დოოკუმენტი წარმოდგენილია javascript-ის ობიექტის სახით*/

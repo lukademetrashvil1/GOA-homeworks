1405c5cc37b97db635654c3687176ed29e15d619
lukademetrashvil1 <demetrashvililuka9@gmail.com>
2025-01-13 13:39:02 +0400

    commit

---
+++ level 71/homework/hw.py
@@ -0,0 +1,7 @@
+def is_sorted_and_how(arr):
+    if all(arr[i] <= arr[i+1] for i in range(len(arr)-1)):
+        return "yes, ascending"
+    elif all(arr[i] >= arr[i+1] for i in range(len(arr)-1)):
+        return "yes, descending"
+    else:
+        return "no"
